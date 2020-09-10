e51189d599952bac185b5a595fba5897d8faee33
Nick <npvisual@users.noreply.github.com>
2020-09-10 09:42:22 -0500

    First implementation of View switching with simple @State variable.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var viewSelection: Bool = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Button(
+                action: { self.viewSelection.toggle() },
+                label: { Text("Change view") }
+            )
+            if viewSelection {
+                Text("Hello World 1 !")
+            } else {
+                Text("Hello World 2 !")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()        }
     }
 }
