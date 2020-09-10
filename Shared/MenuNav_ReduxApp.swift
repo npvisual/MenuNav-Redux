0e98c58a20e8501a2ffbfb3c098ef5f41c82c39e
Nick <npvisual@users.noreply.github.com>
2020-09-10 09:18:07 -0500

    Initial Commit

---
+++ Shared/MenuNav_ReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuNav_ReduxApp.swift
+//  Shared
+//
+//  Created by Nicolas Philippe on 9/10/20.
+//
+
+import SwiftUI
+
+@main
+struct MenuNav_ReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
